b103a0b3cf02fd39cda3e82cf67c9d1094775277
Sam Power <sam.g.power@gmail.com>
2019-03-11 20:44:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const driver = {};
+
+// need to create a function that takes in driver, key and value and returns a new driver that has an updated value for the key
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = {...driver};
+  newDriver[key] = value;
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, name) {
+  const newDriver = {...driver};
+  delete newDriver.name;
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, name) {
+  delete driver.name;
+  return driver;
+}
