02e4006fbc8663c6b8315878651f865248fb7e31
Sam Power <sam.g.power@gmail.com>
2019-03-11 20:14:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key, value) {
   const newDriver = {...driver};
-  delete driver.key;
+  delete newDriver.key;
   
 }
